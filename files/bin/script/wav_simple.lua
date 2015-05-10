643509f8f0a15d062d2e8f4fa062f40243a42d48
Iwahara Hiroaki <rockfieldlab@gmail.com>
2015-05-11 01:24:57 +0900

    Makefile追加
    make未確認

--- files/bin/script/wav_simple.lua
+++ files/bin/script/wav_simple.lua
@@ -3,31 +3,39 @@
 -- >dofile("wav.lua")
 
 -- ライブラリロード
-dofile("lib/mylib.lua")
+dofile("script/mylib.lua")
 
 -- ファイルオープン＆初期化
-file_name = file_name or "test.wav"
+file_name = arg1 or "test.wav"
 stream = init_stream(file_name)
 print_status()
 
+dump(255)
+
 -- ストリーム解析
-B("'RIFF'",                       4)
-B("file_size+muns8",              4)
-B("'wave'",                       4)
-B("'fmt_'",                       4)
-B("size_fmt",                     4)
-B("format_id",                    2)
-B("num_channels",                 2)
-B("sampleing_frequency",          4)
-B("byte_per_sec",                 4)
-B("block_size(smaple_x_channel)", 2)
-B("bit_depth",                    2)
-B("'data'",                       4)
-B("size_audio_data",              4, data)
-B("audio_data",                   reverse_32(data["size_audio_data"]))
-B("tag",                          4)
-B("size_data",                    4, data)
-B("data",                         reverse_32(data["size_data"]))
+local data = {}
+cstr("'RIFF'",                       4, "RIFF")
+byte("file_size+muns8",              4)
+byte("'wave'",                       4)
+byte("'fmt_'",                       4)
+byte("size_fmt",                     4)
+byte("format_id",                    2)
+byte("num_channels",                 2)
+byte("sampleing_frequency",          4)
+byte("byte_per_sec",                 4)
+byte("block_size(smaple_x_channel)", 2)
+byte("bit_depth",                    2)
+byte("'data'",                       4)
+byte("size_audio_data",              4, data)
+
+-- PCMを書き出す
+write("out.pcm",                     reverse_32(data["size_audio_data"]))
+byte("audio_data",                   reverse_32(data["size_audio_data"]))
+
+byte("tag",                          4)
+byte("size_data",                    4, data)
+byte("data",                         reverse_32(data["size_data"]))
+
 
 print_status()
 

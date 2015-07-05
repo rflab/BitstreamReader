cd808bf5eacbac86977b1a20b0c9190280031b49
Iwahara Hiroaki <rockfieldlab@gmail.com>
2015-07-06 01:46:57 +0900

    WAVファイルでフラグチェックするように変更

--- files/bin/script/streamdef/wav.lua
+++ files/bin/script/streamdef/wav.lua
@@ -1,14 +1,9 @@
 -- wav解析
-local __stream_path__ = argv[1] or "test.wav"
-seek(0)
 little_endian(true)
-dump()
-
 cstr ("'RIFF'",                       4, "RIFF")
---rbyte("'RIFF'",                       4)
 rbyte("file_size+muns8",              4)
-rbyte("'wave'",                       4)
-rbyte("'fmt_'",                       4)
+cstr ("'WAVE'",                       4, "WAVE")
+cstr ("'fmt '",                       4, "fmt ")
 rbyte("size_fmt",                     4)
 rbyte("format_id",                    2)
 rbyte("num_channels",                 2)
@@ -16,11 +11,10 @@ rbyte("sampleing_frequency",          4)
 rbyte("byte_per_sec",                 4)
 rbyte("block_size(smaple_x_channel)", 2)
 rbyte("bit_depth",                    2)
-rbyte("'data'",                       4)
+cstr ("'LIST'",                       4, "LIST")
 rbyte("size_audio_data",              4)
 tbyte("pcm",                          get("size_audio_data"), "pcm.dat") -- PCMを書き出す
-rbyte("tag",                          4)
-rbyte("size_data",                    4, data)
+rstr ("tag",                          4)
+rbyte("size_data",                    4)
 rbyte("data",                         get("size_data"))
-
 print_status()
